a3098f975ec4dddecb6cb4b8c3a7a36fa8ba0777
mcparfer <mcparfer@gmail.com>
2023-08-26 19:43:28 +0200

    google cloud deployment x2

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
-docker build -t mcparfer/multi-client -f ./client/Dockerfile ./client
-docker build -t mcparfer/multi-server -f ./server/Dockerfile ./server
-docker build -t mcparfer/multi-worker -f ./worker/Dockerfile ./worker
-docker push mcparfer/multi-client
-docker push mcparfer/multi-server
-docker push mcparfer/multi-worker
+docker build -t mcparfer/multi-client:latest -t mcparfer/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t mcparfer/multi-server:latest -t mcparfer/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t mcparfer/multi-worker:latest -t mcparfer/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+docker push mcparfer/multi-client:latest
+docker push mcparfer/multi-server:latest
+docker push mcparfer/multi-worker:latest
+docker push mcparfer/multi-client:$SHA
+docker push mcparfer/multi-server:$SHA
+docker push mcparfer/multi-worker:$SHA
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=mcparfer/multi-server
+kubectl set image deployments/server-deployment server=mcparfer/multi-server:$SHA
+kubectl set image deployments/client-deployment client=mcparfer/multi-client:$SHA
+kubectl set image deployments/worker-deployment worker=mcparfer/multi-worker:$SHA
